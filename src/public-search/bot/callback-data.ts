25dba184ab1d893e1488b779803e0400bf23bd53
abbie848 <neilabbie123@gmail.com>
2026-05-25 18:42:55 +0800

    chore: remove old root public search source

--- src/public-search/bot/callback-data.ts
+++
@@ -1,8 +0,0 @@
-export function encodeSeasonCallback(seasonId: number) {
-  return `season:${seasonId}`;
-}
-
-export function decodeSeasonCallback(value: string) {
-  const match = /^season:(\d+)$/.exec(value);
-  return match ? Number(match[1]) : undefined;
-}
